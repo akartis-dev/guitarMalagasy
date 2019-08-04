379fb405783ac31270c5d9fb183d00e3b4f54ee5
Sitraka Leon <sitraka@leon.mg>
2019-08-04 16:26:12 +0400

    ajout du style global pour avoir une seule component pour gerer notre style identique

---
+++ components/ReUseComponent/StyleGlobal.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+
+    //rajoute de la marge a notre vue
+    containerMargin : {
+        flex : 1,
+        marginLeft : 5,
+        marginRight : 5
+    },
+
+    titreHira : {
+        fontSize : 25,
+        textAlign : 'left',
+        color : '#707070',
+        fontFamily : "Poppins-Regular"
+    },
+
+    artiste : {
+        fontSize : 15,
+        textAlign : 'left',
+        color : '#D1D1D1',
+        fontFamily : "Poppins-Light"
+    }
+
+})
